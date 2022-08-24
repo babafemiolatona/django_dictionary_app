269e00bfa0d7f0ccb4afc60b459f75d15207929c
hgfemi <femiolatona@gmail.com>
2022-08-24 20:21:54 +0200

    Ready for deployment to heroku.

---
+++ dictionary/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+from PyDictionary import PyDictionary
+
+
+def index(request):
+    return render(request, 'dictionary/index.html')
+
+def search(request):
+    word = request.GET.get('search')
+    dictionary = PyDictionary()
+    meanings = dictionary.meaning(word)
+    synonyms = dictionary.synonym(word)
+    antonyms = dictionary.antonym(word)
+
+    context = {
+        'word': word,
+        'meanings': meanings,
+        'synonyms': synonyms,
+        'antonyms': antonyms
+    }
+
+    return render(request, 'dictionary/search.html', context)
